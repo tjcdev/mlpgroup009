d521e4c64b03c625d23c1848bc16463a9325937f
Daniel Miskell <[s1876926]@sms.ed.ac.uk>
2019-03-11 10:07:49 +0000

    changes to load_model and graph an experiment notebook

--- load_model.py
+++ load_model.py
@@ -1,3 +1,17 @@
 import gym
 import tensorflow as tf
+import functools
 
+from baselines.common.tf_util import get_session, save_variables, load_variables
+
+
+def load(sess, load_path):
+    """
+    Load the model
+    """
+    saver = tf.train.Saver()
+    print('Loading ' + load_path)
+    saver.restore(sess, load_path)
+
+with tf.Session() as sess:
+    model =  load(sess,'./baseline_weights/ppo2_bip')
